eaded12a8595cf1c3abda3ceb89da1d64be4d3da
abhishek dhakad <dhakaddeepak9340700360@gmail.com>
2025-03-17 14:38:04 +0530

    theory max marks subject select or unselect using by checkbox implementation

--- src/app/pages/admin/admin-student-marksheet-structure-edit/admin-student-marksheet-structure-edit.component.ts
+++ src/app/pages/admin/admin-student-marksheet-structure-edit/admin-student-marksheet-structure-edit.component.ts
@@ -1,3 +1,8 @@
+
+
+
+
+
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
@@ -40,17 +45,24 @@ export class AdminStudentMarksheetStructureEditComponent implements OnInit {
     this.getSingleMarksheetTemplateById();
   }
 
+  // ✅ Marksheet Data ko fetch karna
   getSingleMarksheetTemplateById() {
-    this.examResultStructureService.getSingleMarksheetTemplateById(this.id).subscribe((res: any) => {
-      this.examStructure = res.examStructure;
-      this.subjects = res.subjects.map((subject: any) => subject.subject);
-      this.selectedTheoryMaxMarks = res.examStructure.term1.theoryMaxMarks.map(
-        (item: any) => Object.keys(item)[0]
-      );
-      this.patch(); // ✅ Form patching yahi kar rahe hain
-    });
+    this.examResultStructureService.getSingleMarksheetTemplateById(this.id).subscribe(
+      (res: any) => {
+        this.examStructure = res.examStructure;
+        this.subjects = res.subjects.map((subject: any) => subject.subject);
+        this.selectedTheoryMaxMarks = res.examStructure.term1.theoryMaxMarks.map(
+          (item: any) => Object.keys(item)[0]
+        );
+        this.patch(); // ✅ Form ko patch karo
+      },
+      (err) => {
+        this.toastr.error('Failed to fetch marksheet template');
+      }
+    );
   }
 
+  // ✅ Subject ko add/remove karna
   theoryMaxMarks(option: string, event: any) {
     if (event.checked) {
       if (!this.selectedTheoryMaxMarks.includes(option)) {
@@ -58,143 +70,68 @@ export class AdminStudentMarksheetStructureEditComponent implements OnInit {
       }
     } else {
       this.selectedTheoryMaxMarks = this.selectedTheoryMaxMarks.filter(
-        (cls) => cls !== option
+        (subject) => subject !== option
       );
     }
-    this.patch(); // ✅ Form ko har update par patch karo
+    this.patch(); // ✅ Form ko sync karo
   }
 
-  isTheoryMaxMarksSelected(option: string): boolean {
-    return this.selectedTheoryMaxMarks.includes(option);
-  }
-
+  // ✅ Form ko patch karna
   patch() {
     const controlOne = <FormArray>(
       this.subjectPermissionForm.get('type.theoryMaxMarksPermission')
     );
-    controlOne.clear(); // ✅ Purane values ko clear kar do
-    this.selectedTheoryMaxMarks.forEach((x) => {
-      controlOne.push(this.patchTheoryMaxMarksValues(x));
+
+    // ✅ Pehle se added values ko store karo
+    const existingValues = controlOne.controls.reduce((acc: any, curr: any) => {
+      const key = Object.keys(curr.value)[0];
+      acc[key] = curr.value[key];
+      return acc;
+    }, {});
+
+    // ✅ Purane aur naye subjects ko sync karo
+    this.selectedTheoryMaxMarks.forEach((subject) => {
+      const existingIndex = this.selectedTheoryMaxMarks.indexOf(subject);
+
+      if (existingIndex !== -1 && existingValues[subject] !== undefined) {
+        // ✅ Agar subject pehle se available hai to value ko update karo
+        controlOne.at(existingIndex)?.patchValue({
+          [subject]: existingValues[subject],
+        });
+      } else {
+        // ✅ Agar naye subject ko add kar rahe hain to default value leke push karo
+        const existingValue = this.examStructure?.term1?.theoryMaxMarks.find(
+          (item: any) => Object.keys(item)[0] === subject
+        )?.[subject] || '';
+
+        controlOne.push(
+          this.fb.group({
+            [subject]: [existingValue, Validators.required],
+          })
+        );
+      }
     });
+
+    // ✅ Agar koi subject uncheck kar diya ho to use remove karo
+    for (let i = controlOne.length - 1; i >= 0; i--) {
+      const key = Object.keys(controlOne.at(i).value)[0];
+      if (!this.selectedTheoryMaxMarks.includes(key)) {
+        controlOne.removeAt(i);
+      }
+    }
   }
 
-  patchTheoryMaxMarksValues(theoryMaxMarksPermission: any) {
-    return this.fb.group({
-      [theoryMaxMarksPermission.toString()]: [
-        theoryMaxMarksPermission,
-        Validators.required,
-      ],
-    });
+  // ✅ Checkbox ke liye selected state ko maintain karo
+  isTheoryMaxMarksSelected(option: string): boolean {
+    return this.selectedTheoryMaxMarks.includes(option);
   }
 
+  // ✅ Form ko submit karna
   subjectPermissionAdd() {
     this.subjectPermissionForm.value._id = this.id;
     console.log(this.subjectPermissionForm.value);
 
-    // ✅ Yaha API ko call karo (example ke liye commented rakha hai)
-    // this.examResultStructureService.addPermission(this.subjectPermissionForm.value).subscribe(
-    //   (res) => {
-    //     this.toastr.success('Permission added successfully');
-    //   },
-    //   (err) => {
-    //     this.toastr.error('Failed to add permission');
-    //   }
-    // );
+    
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-// import { Component, Inject, OnInit, PLATFORM_ID, AfterViewInit, ElementRef } from '@angular/core';
-// import { ActivatedRoute } from '@angular/router';
-// import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
-// import { AdminAuthService } from 'src/app/services/auth/admin-auth.service';
-// import { ExamResultStructureService } from 'src/app/services/exam-result-structure.service';
-// import { ToastrService } from 'ngx-toastr';
-
-// @Component({
-//   selector: 'app-admin-student-marksheet-structure-edit',
-//   templateUrl: './admin-student-marksheet-structure-edit.component.html',
-//   styleUrls: ['./admin-student-marksheet-structure-edit.component.css']
-// })
-// export class AdminStudentMarksheetStructureEditComponent implements OnInit {
-//   subjectPermissionForm: FormGroup;
-//   adminId: string = '';
-//   id: any;
-//   examStructure: any;
-//   subjects: any[] = [];
-//   selectedTheoryMaxMarks: any[] = [];
-//   constructor(private fb: FormBuilder, public activatedRoute: ActivatedRoute, private toastr: ToastrService, private adminAuthService: AdminAuthService, private examResultStructureService: ExamResultStructureService) {
-//     this.subjectPermissionForm = this.fb.group({
-//       _id: [''],
-//       type: this.fb.group({
-//         theoryMaxMarksPermission: this.fb.array([],[Validators.required]),
-//       }),
-//     });
-//   }
-
-//   ngOnInit(): void {
-//     let getAdmin = this.adminAuthService.getLoggedInAdminInfo();
-//     this.adminId = getAdmin?.id;
-//     this.id = this.activatedRoute.snapshot.paramMap.get('id');
-//     this.getSingleMarksheetTemplateById();
-//   }
-//   getSingleMarksheetTemplateById() {
-//     this.examResultStructureService.getSingleMarksheetTemplateById(this.id).subscribe((res: any) => {
-//       this.examStructure = res.examStructure;
-//       this.subjects = res.subjects.map(((subject: any) => { return subject.subject }));
-//       this.selectedTheoryMaxMarks = res.examStructure.term1.theoryMaxMarks.map((item: any) => {
-//         return Object.keys(item)[0];
-//       });
-//     })
-//   }
-//   theoryMaxMarks(option: number, event: any) {
-//     if (event.checked) {
-//       if (!this.selectedTheoryMaxMarks.includes(option)) {
-//         this.selectedTheoryMaxMarks.push(option);
-//         // console.log(this.selectedTheoryMaxMarks)
-//       }
-//     } else {
-//       this.selectedTheoryMaxMarks = this.selectedTheoryMaxMarks.filter(cls => cls !== option);
-//       // console.log(this.selectedTheoryMaxMarks)
-//     }
-//   }
-//   isTheoryMaxMarksSelected(option: number): boolean {
-//     this.patch();
-//     return this.selectedTheoryMaxMarks.includes(option);
-//   }
-//   patch() {
-//     const controlOne = <FormArray>this.subjectPermissionForm.get('type.theoryMaxMarksPermission');
-//     this.selectedTheoryMaxMarks.forEach((x: any) => {
-//       controlOne.push(this.patchTheoryMaxMarksValues(x))
-//       this.subjectPermissionForm.reset();
-//     })
-//   }
-//   patchTheoryMaxMarksValues(theoryMaxMarksPermission: any) {
-//     return this.fb.group(
-//       { [theoryMaxMarksPermission]: [theoryMaxMarksPermission] }
-//     )
-//   }
-//   subjectPermissionAdd() {
-//     this.subjectPermissionForm.value._id = this.id;
-//     console.log(this.subjectPermissionForm.value)
-  
-//     // this.teacherService.addTeacherPermission(this.teacherPermissionForm.value).subscribe((res: any) => {
-//     //   if (res) {
-//     //     this.successDone(res);
-//     //   }
-//     // }, err => {
-//     //   this.errorCheck = true;
-//     //   this.errorMsg = err.error;
-//     // })
-//   }
-// }
